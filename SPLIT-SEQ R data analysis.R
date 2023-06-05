77224b651f86300635a6b6abaa11b9a27c0be5cc
YannVRB <69206510+YannVRB@users.noreply.github.com>
2023-06-05 13:37:22 -0500

    Update SPLIT-SEQ R data analysis.R

--- SPLIT-SEQ R data analysis.R
+++ SPLIT-SEQ R data analysis.R
@@ -1,4 +1,4 @@
-###This assumes you split-pipe twice as previously explained. Outputs from split-pipe are stored in folder called DGE_filtered
+###This assumes you ran split-pipe twice as previously explained. Outputs from split-pipe are stored in folder called DGE_filtered.
 
 #Loading librairies
 library(Seurat)
@@ -11,45 +11,6 @@ mat_path <- "DGE_filtered/"
 
 #The Seurat function ReadParseBio() provides a convenient way to read your expression matrix into R using the DGE folder path as input.
 mat <- ReadParseBio(mat_path)
-fig_path <- "Figures/"
-SaveFigure <- function(plots, name, type = "png", width, height, res){
-  if(type == "png") {
-    png(paste0(fig_path, name, ".", "png"),
-        width = width, height = height, units = "in", res = 200)
-  } else {
-    pdf(paste0(fig_path, name, ".", "pdf"),
-        width = width, height = height)
-  }
-  print(plots)
-  dev.off()
-}
-SaveObject <- function(object, name){
-  saveRDS(object, paste0(data_path, name, ".RDS"))
-}
-ReadObject <- function(name){
-  readRDS(paste0(data_path, name, ".RDS"))
-}
-cleanUMAP <- function(plot_obj, dark = FALSE, axis_title_size = 16,
-                      plot_title = "") {
-  if (dark == TRUE) {
-    plot_obj <- plot_obj + labs(title = element_text(plot_title), x = "UMAP 1", y = "UMAP 2") +
-      theme(plot.background = element_rect(fill = "black",
-                                           colour = "black", size = 0.5, linetype = "solid"),
-            axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-            axis.line.x = element_blank(), axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(), axis.line.y = element_blank(),
-            axis.title = element_text(size = axis_title_size, color = "white"),
-            legend.text = element_text(color = c("white"), size = 10)) +
-      ggtitle(plot_title)
-  } else {
-    plot_obj <- plot_obj + labs(title = element_text(plot_title), x = "UMAP 1", y = "UMAP 2") +
-      theme(plot.title = element_text(plot_title), axis.text.x = element_blank(),
-            axis.ticks.x = element_blank(), axis.line.x = element_blank(),
-            axis.text.y = element_blank(), axis.ticks.y = element_blank(),
-            axis.line.y = element_blank(),
-            axis.title = element_text(size = axis_title_size))
-  }
-}
 
 #Check to see if empty gene names are present, add name if so
 table(rownames(mat) == "")
@@ -104,3 +65,34 @@ seur_obj <- BuildClusterTree(seur_obj, reorder.numeric = TRUE)
 seur_obj <- RunUMAP(seur_obj, reduction = "pca", dims = 1:20)
 DimPlot(seur_obj, reduction = "umap", group.by = "orig.ident")
 
+#Assessing the expression of bmarkers in specific clusters
+markers <- c("Ahcyl2", "Gm10754", "Nectin3", "Rgs20", "Gad2", "Mog", "Tshz2",
+"Pdgfra", "Il16", "Cp", "Inpp5d", "Filip1l", "Trp73", "Slc6a13")
+FeaturePlot(pbmc, features = markers)
+
+#Generating a list where each cell type is assigned multiple clusters so they can be merged
+new_ids <- c("Dentate Gyrus", "CA1", "CA3", "Astrocytes", "Interneurons",
+  "Oligodendrocytes", "Excitatory cortical neurons", "CA1-ProS", "OPC", "CA2", "Endothelial", "Microglia", "SMC-Peri", "Cajal–Retzius", "VLMC")
+
+new_id_list <- list(Dentate Gyrus = c(0,2,16), CA1 = 1,
+  CA3 = 3, Astrocytes = c(4,10), Interneurons = c(5,13), Oligodendrocytes = 6, Excitatory cortical neurons = c(7,8),
+  CA1-ProS = 9, OPC = 11, CA2 = 12, Endothelial = 14, Microglia = 15, SMC-Peri = 17, Cajal–Retzius = 18, VLMC = 19)
+
+for (i in 1:length(new_id_list)) {
+  ind <- which(seur_obj@meta.data$RNA_snn_res.0.3 %in% new_id_list[[i]])
+  seur_obj@meta.data$collapsed[ind] <- names(new_id_list)[i]
+}
+
+seur_obj@meta.data$collapsed <- factor(
+  seur_obj@meta.data$collapsed, levels = names(new_id_list), ordered = TRUE)
+Idents(seur_obj) <- pbmc@meta.data$collapsed
+
+names(new_ids) <- levels(seur_obj)
+seur_obj <- RenameIdents(seur_obj, new_ids)
+
+#Differential gene expression analysis in Oligodendrocytes
+seur_obj$celltype.sample_category <- paste(Idents(seur_obj), seur_obj$sample_category, sep = "_")
+seur_obj$celltype <- Idents(seur_obj)
+Idents(seur_obj) <- "celltype.sample_category"
+
+oligo.DEG <- FindMarkers(seur_obj, ident.1 = "Oligodendrocytes_SOR", ident.2 = "Oligodendrocytes_HC", min.pct=0, logfc.threshold=0)
